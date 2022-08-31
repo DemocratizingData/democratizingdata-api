6af0917394341c1ade8881138cd6566867ea78f5
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2022-08-31 10:34:26 -0500

    Fix unit test

--- democratizing/datasets/tests/dataset_router_unit_test.py
+++ democratizing/datasets/tests/dataset_router_unit_test.py
@@ -2,6 +2,15 @@ from unittest.mock import MagicMock
 
 
 def test_datasets(test_client, mock_db):
-    mock_db.query.return_value.all.return_value = [MagicMock(id=1, alias="test")]
+    mock_db.query.return_value.join.return_value.all.return_value = [
+        MagicMock(
+            id=1,
+            agency="test",
+            version="test_version",
+            alias_type="test",
+            url="test",
+            alias="test",
+        )
+    ]
     response = test_client.get("/datasets")
     assert response.status_code == 200
